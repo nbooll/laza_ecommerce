e8fd5aa2616be213bec29cd4e567a134af2df9d7
Nabil Al-refaiy <144157955+nbooll@users.noreply.github.com>
2023-10-02 19:31:05 +0300

    v4

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Reviews(),
+      home: const SplashScreen(),
       scrollBehavior: MyCustomScrollBehavior(),
       debugShowCheckedModeBanner: false,
     );
